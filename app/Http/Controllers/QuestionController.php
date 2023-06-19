637fe57b8b99c6b22602188e2d57fb0c0f24fe37
Keny R C Noleto <kenyrcnoleto@gmail.com>
2023-06-19 11:38:25 -0300

    Hey-11: it should check if ends with question mark?

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Question;
+use Closure;
 use Illuminate\Http\{RedirectResponse, Request};
 
 class QuestionController extends Controller
@@ -17,7 +18,15 @@ class QuestionController extends Controller
         //  $question->save();
 
         $atributes = request()->validate([
-            'question' => ['required', 'min:10'],
+            'question' => [
+                'required',
+                'min:10',
+                function (string $attribute, mixed $value, Closure $fail) {
+                    // dd($value[strlen($value)-1] != '?');
+                    if ($value[strlen($value) - 1] != '?') {
+                        $fail('Are you sure that is a question? It is missing the question mark in the end');
+                    }
+                }, ],
         ]);
 
         Question::query()->create($atributes);
--- tests/Feature/CreateAQuestionTest.php
+++ tests/Feature/CreateAQuestionTest.php
@@ -28,6 +28,24 @@ it('should be able to create a new question bigger than 255 characters', functio
 });
 
 it('should check if ends with question mark?', function () {
+    //Arange :: preparar
+    $user = User::factory()->create();
+
+    actingAs($user);
+
+    //Act :: agir
+
+    $request = post(route(name: 'question.store'), [
+        'question' => str_repeat(string: '*', times: 10),
+
+    ]);
+
+    //Asert :: verificar
+    $request->assertSessionHasErrors([
+        'question' => 'Are you sure that is a question? It is missing the question mark in the end',
+    ]);
+
+    $this->assertDatabaseCount(table: 'questions', count: 0);
 
 });
 
