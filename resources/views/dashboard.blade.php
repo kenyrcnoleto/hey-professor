71bc6f1136564aa02401180c5ed0d6a8eb548038
Keny R C Noleto <kenyrcnoleto@gmail.com>
2024-07-03 11:48:44 -0300

    wip

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -22,16 +22,16 @@
     {{-- Posso passar parâmetro para o blade  --}}
     {{-- proprieedate. colocando : entende que é um código php, mas precisa colocar as   '' --}}
     {{-- Existe essas 3 possibilidade de passar informações para as propriedades dos componentes --}}
-    {{-- <x-rafael param1="oi">
+    {{-- <x-teste param1="oi">
                 <x-slot:title>
                     <div class="text-blue-400">MEU TITULO AZUL</div>
                 </x-slot:title>
                 testeteee
-            </x-rafael>
-            <x-rafael title="diferent"/>
-            <x-rafael  title="legal" />
-            <x-rafael  title="show" />
-            <x-rafael  title="mais um" /> --}}
+            </x-teste>
+            <x-teste title="diferent"/>
+            <x-teste  title="legal" />
+            <x-teste  title="show" />
+            <x-teste  title="mais um" /> --}}
 
 
 </x-app-layout>
