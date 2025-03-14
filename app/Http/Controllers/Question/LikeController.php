25f9470135d038e40eeb50d3eea9a508a2f350c7
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-03-14 11:30:10 -0300

    Hey-14: adding global function file

--- app/Http/Controllers/Question/LikeController.php
+++ app/Http/Controllers/Question/LikeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Question;
 
 use App\Http\Controllers\Controller;
-use App\Models\{Question, Vote};
+use App\Models\{Question, User, Vote};
 use Illuminate\Http\{RedirectResponse, Request};
 
 class LikeController extends Controller
@@ -11,8 +11,16 @@ class LikeController extends Controller
     public function __invoke(Question $question): RedirectResponse
     {
 
-        //Dessa forma facilita na hora de manutenção
-        auth()->user()->like($question);
+        //Pelo fato do auth ser um contrato do tipo authenticable IDE não consegue buscar o método
+        //direto no model. Forma de resolver
+
+        /** @var User $user */
+        $user = auth()->user();
+        //$user->like($question);
+
+        //Dessa forma facilita na hora de manutenção: auth()->user()->like($question);
+        //Foi inserido uma função global
+        user()->like($question);
 
         //Route Model Binding - link entre a rota e o model - dessa forma não precisa procurar no model novamente - procura por padrão findOrFail
         //$question = Question::find($question);
