59b3f59092900e3bbe2a0c8e09c5a46420ba9393
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-04-18 14:22:45 -0300

    Hey-17: it should make sure that only the person who create the question can publish the question

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -17,7 +17,7 @@ class QuestionController extends Controller
         //  $question->question = request()->question;
         //  $question->save();
 
-        $atributes = request()->validate([
+        request()->validate([
             'question' => [
                 'required',
                 'min:10',
@@ -30,10 +30,12 @@ class QuestionController extends Controller
             ],
         ]);
 
-        Question::query()->create([
-            'question' => request()->question,
-            'draft'    => true,
-        ]);
+        //Question::query()
+        user()->questions()
+            ->create([
+                'question' => request()->question,
+                'draft'    => true,
+            ]);
 
         return to_route('dashboard');
     }
--- tests/Feature/Question/PublishTest.php
+++ tests/Feature/Question/PublishTest.php
@@ -8,7 +8,9 @@ test('it should be able to publish a question', function () {
 
     $user = User::factory()->create();
 
-    $question = Question::factory()->create(['draft' => true]);
+    $question = Question::factory()
+        ->for($user, 'createdBy')
+        ->create(['draft' => true]);
 
     actingAs($user);
 
@@ -20,5 +22,24 @@ test('it should be able to publish a question', function () {
 
     expect($question)
         ->draft->toBeFalse();
+
+});
+
+test('it should make sure that only the person who create the question can publish the question ', function () {
+    $rightUser = User::factory()->create();
+    $wrongUser = User::factory()->create();
+
+    $question = Question::factory()->create(['draft' => true, 'created_by' => $rightUser->id]);
+
+    actingAs($wrongUser);
+
+    //Forbidden - não permitido a publicação pelo usuario errado
+    put(route('question.publish', $question))
+        ->assertForbidden();
+
+    actingAs($rightUser);
+
+    put(route('question.publish', $question))
+        ->assertRedirect();
 
 });
