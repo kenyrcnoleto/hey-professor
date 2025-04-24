f2bd6412e636963124b34f59d8aca1b56f4968bd
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-04-24 11:36:08 -0300

    Hey-17: it should be able to destroy a question

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -20,31 +20,87 @@
         <hr class="border-gray-700 border-dashed my-4">
 
         {{-- listatagem --}}
-        <div class="dark: text-gray-500 uppercase font-bold mb-1" >My Questions</div>
+        <div class="dark: text-gray-500 uppercase font-bold mb-1">My Drafts</div>
         {{-- @dd($questions) --}}
-        <div class="dark: text-gray-400 space-y-4" >
-            @foreach ($questions as $item)
+        <div class="dark: text-gray-400 space-y-4">
 
-                <x-question :question=$item />
+            <x-table>
+                <x-table.thead>
+                    <tr>
+                        <x-table.th>
+                            Questions
+                        </x-table.th>
 
-            @endforeach
+                        <x-table.th>
+                            Actions
+                        </x-table.th>
+                    </tr>
+                </x-table.thead>
+                <tbody>
+                    @foreach ($questions->where('draft', true) as $question)
+                        <x-table.tr>
+                            <x-table.td>
+                                {{ $question->question }}
+                            </x-table.td>
+                            <x-table.td>
+                                <x-form :action="route('question.destroy', $question)" delete>
+                                    <button type="submit" class="hover:underline text-blue-500">
+                                        Delete
+                                    </button>
+                                </x-form>
+
+                                <x-form :action="route('question.publish', $question)" put>
+                                    <button type="submit" class="hover:underline text-blue-500">
+                                        Publicar
+                                    </button>
+                                </x-form>
+                            </x-table.td>
+                        </x-table.tr>
+                    @endforeach
+                </tbody>
+            </x-table>
+
+        </div>
+
+         <hr class="border-gray-700 border-dashed my-4">
+
+        {{-- listatagem --}}
+        <div class="dark: text-gray-500 uppercase font-bold mb-1">My Questions</div>
+        {{-- @dd($questions) --}}
+        <div class="dark: text-gray-400 space-y-4">
+
+            <x-table>
+                <x-table.thead>
+                    <tr>
+                        <x-table.th>
+                            Questions
+                        </x-table.th>
+
+                        <x-table.th>
+                            Actions
+                        </x-table.th>
+                    </tr>
+                </x-table.thead>
+                <tbody>
+                    @foreach ($questions->where('draft', false) as $question)
+                        <x-table.tr>
+                            <x-table.td>
+                                {{ $question->question }}
+                            </x-table.td>
+                            <x-table.td>
+                                <x-form :action="route('question.destroy', $question)" delete>
+                                    <button type="submit" class="hover:underline text-blue-500">
+                                        Delete
+                                    </button>
+                                </x-form>
+                                //botão de publicar
+                            </x-table.td>
+                        </x-table.tr>
+                    @endforeach
+                </tbody>
+            </x-table>
+
         </div>
     </x-container>
-
-
-    {{-- Posso passar parâmetro para o blade  --}}
-    {{-- proprieedate. colocando : entende que é um código php, mas precisa colocar as   '' --}}
-    {{-- Existe essas 3 possibilidade de passar informações para as propriedades dos componentes --}}
-    {{-- <x-teste param1="oi">
-                <x-slot:title>
-                    <div class="text-blue-400">MEU TITULO AZUL</div>
-                </x-slot:title>
-                testeteee
-            </x-teste>
-            <x-teste title="diferent"/>
-            <x-teste  title="legal" />
-            <x-teste  title="show" />
-            <x-teste  title="mais um teste" /> --}}
-
 
 </x-app-layout>
