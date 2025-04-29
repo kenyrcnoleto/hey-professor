3dcc2085459a76d7cc91dc2b3c5729626aba7726
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-04-29 13:36:10 -0300

    Hey-21: pagination dasboard results

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', DashboardController::class)->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('/dashboard', DashboardController::class)->name('dashboard');
 
     #region Question Routes
     Route::get('/question', [QuestionController::class, 'index'])->name('question.index');
