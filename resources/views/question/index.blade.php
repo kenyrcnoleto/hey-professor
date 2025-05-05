fc0ae85ef8ca38d58de5a63835dffbcde70dcdb3
Keny R C Noleto <kenyrcnoleto@gmail.com>
2025-05-05 13:49:59 -0300

    Hey-23: implementing the frontend for the archive/restore options

--- resources/views/question/index.blade.php
+++ resources/views/question/index.blade.php
@@ -43,7 +43,7 @@
                                 {{ $question->question }}
                             </x-table.td>
                             <x-table.td>
-                                <x-form :action="route('question.destroy', $question)" delete>
+                                <x-form :action="route('question.destroy', $question)" delete onsubmit="return confirm('Tem certeza?')">
                                     <button type="submit" class="hover:underline text-blue-500">
                                         Delete
                                     </button>
@@ -90,12 +90,52 @@
                                 {{ $question->question }}
                             </x-table.td>
                             <x-table.td>
-                                <x-form :action="route('question.destroy', $question)" delete>
+                                <x-form :action="route('question.destroy', $question)" delete onsubmit="return confirm('Tem certeza?')">
                                     <button type="submit" class="hover:underline text-blue-500">
                                         Delete
                                     </button>
                                 </x-form>
-                                //botão de publicar
+                                <x-form :action="route('question.archive', $question)" patch>
+                                    <button type="submit" class="hover:underline text-blue-500">
+                                        Archive
+                                    </button>
+                                </x-form>
+                            </x-table.td>
+                        </x-table.tr>
+                    @endforeach
+                </tbody>
+            </x-table>
+
+        </div>
+
+        <div class="dark: text-gray-500 uppercase font-bold mb-1">Archived Questions</div>
+        {{-- @dd($questions) --}}
+        <div class="dark: text-gray-400 space-y-4">
+
+            <x-table>
+                <x-table.thead>
+                    <tr>
+                        <x-table.th>
+                            Questions
+                        </x-table.th>
+
+                        <x-table.th>
+                            Actions
+                        </x-table.th>
+                    </tr>
+                </x-table.thead>
+                <tbody>
+                    @foreach ($archivedQuestions->where('draft', false) as $question)
+                        <x-table.tr>
+                            <x-table.td>
+                                {{ $question->question }}
+                            </x-table.td>
+                            <x-table.td>
+                                <x-form :action="route('question.restore', $question)" patch>
+                                    <button type="submit" class="hover:underline text-blue-500">
+                                        Restore
+                                    </button>
+                                </x-form>
                             </x-table.td>
                         </x-table.tr>
                     @endforeach
